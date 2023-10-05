c7a52ca6e7b5f31afd7a96666207975a776d3777
Alex <alexander.unmuth@gmail.com>
2023-10-05 10:57:49 +0200

    Big Bang!

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -1,7 +1,12 @@
 package de.neuefische;
+import static de.neuefische.StringReverser.reverse;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Hauptmethode zum Testen der reverse-Methode
+        public static void main(String[] args) {
+            // Testzeichenkette
+            String testString = "Hallo";
+            // Aufruf der reverse-Methode und Ausgabe des Ergebnisses
+            System.out.println(StringReverser.reverse(testString));  // Ausgabe: "ollaH"
+        }
     }
-}
